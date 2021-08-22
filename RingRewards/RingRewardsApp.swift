a2d1767d8a575e7c7f5fee91c1aecae96e3ab1bf
CaptainZ1 <iainstuart5@gmail.com>
2021-08-22 10:00:45 -0400

    Initial Commit

---
+++ RingRewards/RingRewardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RingRewardsApp.swift
+//  RingRewards
+//
+//  Created by Iain Zwiebel on 8/22/21.
+//
+
+import SwiftUI
+
+@main
+struct RingRewardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
